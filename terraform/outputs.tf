d7c4318bd771c3690bcc08f8d19779d4bd239726
David <davidblitx@gmail.com>
2026-07-12 20:42:42 +0100

    feat: add changes outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,19 @@
 output "vpc_id" {
-    description = "The ID of the VPC"
-    value = module.network.vpc_id
+  description = "The ID of the VPC"
+  value       = module.network.vpc_id
 }
 
 output "public_subnet_ids" {
-    description = "Public subnet IDs"
-    value       = module.network.public_subnet_ids
+  description = "Public subnet IDs"
+  value       = module.network.public_subnet_ids
 }
 
 output "private_subnet_ids" {
-    description = "Private subnet IDs"
-    value = module.network.private_subnet_ids
+  description = "Private subnet IDs"
+  value       = module.network.private_subnet_ids
 }
 
 output "alb_dns_name" {
   description = "The public DNS name of the Application Load Balancer"
   value       = module.compute.alb_dns_name
-}
+}   
