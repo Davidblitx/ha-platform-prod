61847a84295d7bd3134d022d01616479d364131f
David <davidblitx@gmail.com>
2026-07-12 20:47:50 +0100

    feat: add changes to security/outputs.tf

--- terraform/modules/security/outputs.tf
+++ terraform/modules/security/outputs.tf
@@ -6,7 +6,7 @@ output "alb_security_group_id" {
 output "ec2_security_group_id" {
   description = "The ID of the security group for the EC2 instances"
   value       = aws_security_group.ec2.id
-}   
+}
 
 output "ssm_security_group_id" {
   description = "The ID of the security group for SSM endpoints"
