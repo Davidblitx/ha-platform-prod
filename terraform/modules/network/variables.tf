80be2f44f7542a9d6b4e1d2d170c16dd658a3e63
David <davidblitx@gmail.com>
2026-07-12 20:44:43 +0100

    feat: add changes to network/variables.tf

--- terraform/modules/network/variables.tf
+++ terraform/modules/network/variables.tf
@@ -1,29 +1,29 @@
-    variable "env" {
-      description = "The deployment environment (e.g., dev, staging, prod)"
-      type = string
-      default = "dev"
-    }
+variable "env" {
+  description = "The deployment environment (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
 
-    variable "vpc_cidr" {
-      description = "The CIDR block for the VPC"
-      type = string
-      default = "10.0.0.0/16"
-    }
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
 
-    variable "public_subnet_cidrs" {
-      description = "CIDR blocks for the public subnets"
-      type = list(string)
-      default = ["10.0.1.0/24", "10.0.2.0/24"]
-    }
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for the public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
 
-    variable "private_subnet_cidrs" {
-      description = "CIDR blocks for the private subnets"
-      type = list(string)
-      default = ["10.0.11.0/24", "10.0.12.0/24"]
-    }
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for the private subnets"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
 
-    variable "availability_zones" {
-      description = "List of availability zones for the subnets"
-      type = list(string)
-      default = [ "eu-west-1a", "eu-west-1b" ]
-    }
+variable "availability_zones" {
+  description = "List of availability zones for the subnets"
+  type        = list(string)
+  default     = ["eu-west-1a", "eu-west-1b"]
+}
