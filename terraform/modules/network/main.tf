47f866264c545f06497f9fe5894e322d7dcdbe9c
David <davidblitx@gmail.com>
2026-07-12 20:46:23 +0100

    feat: add changes to network/main.tf

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidrs[count.index]
   availability_zone       = var.availability_zones[count.index]
-  map_public_ip_on_launch = true 
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "${var.env}-public-subnet-${count.index + 1}"
@@ -48,13 +48,13 @@ resource "aws_internet_gateway" "igw" {
 # 4. Elastic IP for NAT Gateway
 resource "aws_eip" "nat" {
   domain     = "vpc"
-  depends_on = [ aws_internet_gateway.igw ]
+  depends_on = [aws_internet_gateway.igw]
 
   tags = {
     Name = "${var.env}-nat-eip"
     Env  = var.env
   }
-} 
+}
 
 # 5. NAT Gateway (Side door - placed in the first public subnet)
 resource "aws_nat_gateway" "nat" {
